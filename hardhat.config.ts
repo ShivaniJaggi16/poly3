48d53ad574b4578379c5b71505a05d841edf73a2
ShivaniJaggi16 <137707056+ShivaniJaggi16@users.noreply.github.com>
2023-08-12 20:44:18 +0530

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,8 @@ import "@nomicfoundation/hardhat-toolbox";
 // https://github.com/projectsophon/hardhat-circom
 import "hardhat-circom";
 // circuits
-import circuits = require('./circuits.config.json')
-import 'dotenv/config'
+import circuits = require('./circuits.config.json');
+import 'dotenv/config';
 
 // set env var to the root of the project
 process.env.BASE_PATH = __dirname;
@@ -12,24 +12,22 @@ process.env.BASE_PATH = __dirname;
 // tasks
 import "./tasks/newcircuit.ts"
 
-const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.17",
-      },
-      {
-        version: "0.6.11",
-      }
-    ]
-  },
-  networks :{
-    mumbai: {
-      url: `https://rpc.ankr.com/polygon_mumbai`,
-      accounts: [process.env.MUMBAIPRIVATEKEY]
-    }
-  },
-
+const config: HardhatUserConfig = { 
+  solidity: { 
+    compilers: [ 
+      { 
+        version: "0.8.19", 
+      }, 
+      { 
+        version: "0.6.11", 
+      } 
+    ] 
+  }, 
+  networks:{ 
+  mumbai: { url :`https://rpc.ankr.com/polygon_mumbai`, 
+  accounts: [process.env.MUMBAIPRIVATEKEY]
+ } 
+},
   circom: {
     // (optional) Base path for input files, defaults to `./circuits/`
     inputBasePath: "./circuits",
